9e56f6c5da086c3d27a0feb9247e87017b7ba022
Ebenezer Fuachie <fuachiee717@gmail.com>
2026-04-26 22:42:46 +0000

    feat(auth-crypto): add role-based admin access for user management and crypto creation

--- src/routes/cryptoRoutes.js
+++ src/routes/cryptoRoutes.js
@@ -5,12 +5,13 @@ import {
   getNewListings,
   getTopGainers,
 } from '../controllers/cryptoController.js';
+import { adminOnly, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', getAllCryptos);
 router.get('/gainers', getTopGainers);
 router.get('/new', getNewListings);
-router.post('/', createCrypto);
+router.post('/', protect, adminOnly, createCrypto);
 
 export default router;
